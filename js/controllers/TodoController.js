ac08c637deefdd9288f69f0ea9871eaeab5caf93
Justin James <digitaldrummerj@gmail.com>
2015-09-29 17:01:13 -0700

    added project title to task view title.  added task open/close modal and save task stub

--- js/controllers/TodoController.js
+++ js/controllers/TodoController.js
@@ -2,7 +2,30 @@
 		.module('todoApp')
 		.controller('ToDoController', ToDoController);
 
-	ToDoController.$inject = ['$scope', 'Projects'];
-	function ToDoController($scope, Projects) {
+	ToDoController.$inject = ['$scope', 'Projects', '$ionicModal'];
+	function ToDoController($scope, Projects, $ionicModal) {
+		
+		// Create our modal
+		$ionicModal.fromTemplateUrl('templates/new-task.html', function(modal) {
+			$scope.taskModal = modal;
+		}, {
+			scope: $scope
+		});
 
+		$scope.newTask = function() {
+			$scope.taskModal.show();
+		};
+	
+		$scope.closeNewTask = function() {
+			$scope.taskModal.hide();
+		}
+		
+		$scope.createTask = function (task) {
+			console.log('TodoController.createTask');		
+			// if (!$scope.activeProject || !task) {
+			// 	return;
+			// }
+
+			$scope.taskModal.hide();
+		}
 	}
